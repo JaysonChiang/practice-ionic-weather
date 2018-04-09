2099bd2f0faa03a58e92e0d4e257239992ef2854
Jayson <jayson@mac.local>
2018-04-10 00:52:23 +0800

    ui revise

--- www/js/app.js
+++ www/js/app.js
@@ -3,59 +3,60 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('App', ['ionic'])
-  .config(function ($stateProvider, $urlRouterProvider) {
+angular
+  .module("App", ["ionic"])
+  .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('search', {
-        url: '/search',
-        controller: 'SearchController',
-        templateUrl: 'views/search/search.html'
+      .state("search", {
+        url: "/search",
+        controller: "SearchController",
+        templateUrl: "views/search/search.html"
       })
-      .state('settings', {
-        url: '/settings',
-        controller: 'SettingsController',
-        templateUrl: 'views/settings/settings.html'
+      .state("settings", {
+        url: "/settings",
+        controller: "SettingsController",
+        templateUrl: "views/settings/settings.html"
       })
-      .state('weather', {
-        url: '/weather/:city/:lat/:lng',
-        controller: 'WeatherController',
-        templateUrl: 'views/weather/weather.html'
-      })
+      .state("weather", {
+        url: "/weather/:city/:lat/:lng",
+        controller: "WeatherController",
+        templateUrl: "views/weather/weather.html"
+      });
 
-    $urlRouterProvider.otherwise('/search');
+    $urlRouterProvider.otherwise("/weather/台北/23.03/121.57");
   })
-  .controller('LeftMenuController', function ($scope, Locations) {
+  .controller("LeftMenuController", function($scope, Locations) {
     $scope.locations = Locations.data;
 
-    $scope.getIcon = function (current) {
+    $scope.getIcon = function(current) {
       if (current) {
-        return 'ion-ios-navigate';
+        return "ion-ios-navigate";
       }
-      return 'ion-ios-location';
-    }
+      return "ion-ios-location";
+    };
   })
 
-  .factory('Settings', function () {
+  .factory("Settings", function() {
     var Settings = {
-      units: 'si',
+      units: "si",
       days: 8
     };
     return Settings;
   })
 
-  .factory('Locations', function ($ionicPopup) {
+  .factory("Locations", function($ionicPopup) {
     function store() {
-      localStorage.setItem('locations', angular.toJson(Locations.data));
+      localStorage.setItem("locations", angular.toJson(Locations.data));
     }
 
     var Locations = {
       data: [],
 
       //確定地點在結果列表的索引
-      getIndex: function (item) {
+      getIndex: function(item) {
         console.log(item);
         var index = -1;
-        Locations.data.forEach(function (location, i) {
+        Locations.data.forEach(function(location, i) {
           if (item.lat === location.lat && item.lng === location.lng) {
             index = i;
           }
@@ -64,31 +65,30 @@ angular.module('App', ['ionic'])
       },
 
       // 從結果列表中增加或是刪除
-      toggle: function (item) {
+      toggle: function(item) {
         var index = Locations.getIndex(item);
         if (index >= 0) {
-
-          $ionicPopup.confirm({
-            title: 'Are you sure?',
-            template: 'This will remove ' + Locations.data[index].city
-          })
-            .then(function (res) {
+          $ionicPopup
+            .confirm({
+              title: "Are you sure?",
+              template: "This will remove " + Locations.data[index].city
+            })
+            .then(function(res) {
               if (res) {
                 Locations.data.splice(index, 1);
               }
             });
-
         } else {
           Locations.data.push(item);
           $ionicPopup.alert({
-            title: 'Location saved'
+            title: "Location saved"
           });
         }
         store();
       },
 
       // 如果新增資料，將其移到最頂層或將它增加到最頂層
-      primary: function (item) {
+      primary: function(item) {
         var index = Locations.getIndex(item);
         if (index >= 0) {
           Locations.data.splice(index, 1);
@@ -98,56 +98,70 @@ angular.module('App', ['ionic'])
         }
         store();
       }
-    }
+    };
 
     try {
-      var items = angular.fromJson(localStorage.getItem('locations')) || [];
+      var items = angular.fromJson(localStorage.getItem("locations")) || [];
       Locations.data = items;
     } catch (e) {
-      Locations.data = []
+      Locations.data = [];
     }
 
     return Locations;
   })
 
-  .filter('timezone', function () {
-    return function (input, timezone) {
+  .filter("timezone", function() {
+    return function(input, timezone) {
       if (input && timezone) {
         var time = moment.tz(input * 1000, timezone);
-        return time.format('LT')
+        return time.format("LT");
       }
-      return '';
-    }
+      return "";
+    };
   })
-  .filter('chance', function () {
-    return function (chance) {
+  .filter("chance", function() {
+    return function(chance) {
       if (chance) {
         var value = Math.round(chance / 10);
-        return value * 10
+        return value * 10;
       }
-      return 0
-    }
-  })
-  .filter('icons', function () {
-    var map = {
-      'clear-day': 'ion-ios-sunny',
-      'clear-night': 'ion-ios-moon',
-      rain: 'ion-ios-rainy',
-      snow: 'ion-ios-snowy',
-      sleet: 'ion-ios-rainy',
-      wind: 'ion-ios-flag',
-      fog: 'ion-ios-cloud',
-      cloudy: 'ion-ios-cloudy',
-      'partly-cloudy-day': 'ion-ios-partlysunny',
-      'partly-cloudy-night': 'ion-ios-cloudy-night'
+      return 0;
     };
-    return function (icon) {
-      return map[icon] || '';
-    }
+  })
+  .filter("icons", function() {
+    var map = {
+      "clear-day": "ion-ios-sunny",
+      "clear-night": "ion-ios-moon",
+      rain: "ion-ios-rainy",
+      snow: "ion-ios-snowy",
+      sleet: "ion-ios-rainy",
+      wind: "ion-ios-flag",
+      fog: "ion-ios-cloud",
+      cloudy: "ion-ios-cloudy",
+      "partly-cloudy-day": "ion-ios-partlysunny",
+      "partly-cloudy-night": "ion-ios-cloudy-night"
+    };
+    return function(icon) {
+      return map[icon] || "";
+    };
+  })
+  .filter("toZhTw", function() {
+    var map = {
+      Monday: "週一",
+      Tuesday: "週二",
+      Wednesday: "週三",
+      Thursday: "週四",
+      Friday: "週五",
+      Saturday: "週六",
+      Sunday: "週日"
+    };
+    return function(txt) {
+      return map[txt] || txt;
+    };
   })
 
-  .run(function ($ionicPlatform, $http, $state, Locations) {
-    $ionicPlatform.ready(function () {
+  .run(function($ionicPlatform, $http, $state, Locations) {
+    $ionicPlatform.ready(function() {
       if (window.cordova && window.cordova.plugins.Keyboard) {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -162,12 +176,11 @@ angular.module('App', ['ionic'])
         StatusBar.styleDefault();
       }
 
-
       if ("geolocation" in navigator) {
-
-        navigator.geolocation.getCurrentPosition(function (position) {
-          console.log(position.coords.latitude, position.coords.longitude);
-
+        navigator.geolocation.getCurrentPosition(
+          function(position) {
+            console.log(position.coords.latitude, position.coords.longitude);
+            /*
           $http.get('https://maps.googleapis.com/maps/api/geocode/json', {
             params: { latlng: position.coords.latitude + ',' + position.coords.longitude }
           })
@@ -183,13 +196,14 @@ angular.module('App', ['ionic'])
               $state.go('weather', location)
             }
             );
-
-        }, function (err) {
-          console.log('error', err)
-        });
+*/
+          },
+          function(err) {
+            console.log("error", err);
+          }
+        );
       } else {
         console.log("geolocation IS NOT available");
       }
-
     });
-  })
+  });
--- www/views/weather/weather.js
+++ www/views/weather/weather.js
@@ -1,92 +1,138 @@
-angular.module('App')
-    .controller('WeatherController', function ($scope, $http, $stateParams, $ionicActionSheet, $ionicModal, $ionicLoading, Settings, Locations) {
-        $scope.params = $stateParams;
-        $scope.settings = Settings;
+angular
+  .module("App")
+  .controller("WeatherController", function(
+    $scope,
+    $http,
+    $stateParams,
+    $ionicActionSheet,
+    $ionicModal,
+    $ionicLoading,
+    Settings,
+    Locations
+  ) {
+    $scope.params = $stateParams;
+    $scope.settings = Settings;
 
-        $ionicLoading.show();
-        $http.get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.lng, { params: { units: Settings.units } })
-            .then(function (res) {
-                $scope.forecast = res.data;
-                $ionicLoading.hide();
-            }, function (err) {
-                $ionicLoading.show({
-                    template: err.data,
-                    duration: 3000
-                });
-            })
-
-        //Scroll
-        var barHeight = document.getElementsByTagName('ion-header-bar')[0].clientHeight;
-
-        $scope.getWidth = function () {
-            return window.innerWidth + 'px';
+    $ionicLoading.show();
+    $scope.isLoading = true;
+    $http
+      .get("/api/forecast/" + $stateParams.lat + "," + $stateParams.lng, {
+        params: { units: Settings.units, lang:'zh-tw' }
+      })
+      .then(
+        function(res) {
+          $scope.forecast = res.data;
+          console.log(res.data);
+          $ionicLoading.hide();
+          $scope.isLoading = false;
+        },
+        function(err) {
+          $ionicLoading.show({
+            template: err.data,
+            duration: 3000
+          });
+          $scope.isLoading = true;
         }
+      );
 
-        $scope.getHeight = function () {
-            return parseInt(window.innerHeight - barHeight) + 'px';
+    // $ionicSlides START
+    $scope.options = {
+      loop: false,
+      effect: "slide",
+      speed: 500
+    };
+
+    $scope.$on("$ionicSlides.sliderInitialized", function(event, data) {
+      // data.slider is the instance of Swiper
+      $scope.slider = data.slider;
+    });
+
+    $scope.$on("$ionicSlides.slideChangeStart", function(event, data) {
+      console.log("Slide change is beginning");
+    });
+
+    $scope.$on("$ionicSlides.slideChangeEnd", function(event, data) {
+      // note: the indexes are 0-based
+      $scope.activeIndex = data.slider.activeIndex;
+      $scope.previousIndex = data.slider.previousIndex;
+    });
+    // $ionicSlides END
+
+    //Scroll
+    var barHeight = document.getElementsByTagName("ion-header-bar")[0]
+      .clientHeight;
+
+    $scope.getWidth = function() {
+      return window.innerWidth + "px";
+    };
+
+    $scope.getHeight = function() {
+      return parseInt(window.innerHeight - barHeight) + "px";
+    };
+
+    $scope.getTotalHeight = function() {
+      return parseInt(parseInt($scope.getHeight()) * 3) + "px";
+    };
+
+    $scope.showOptions = function() {
+      var sheet = $ionicActionSheet.show({
+        buttons: [
+          { text: "Toggle Favorite" },
+          { text: "Set as Primary" },
+          { text: "Sunrise Sunset Chart" }
+        ],
+        cancelText: "Cancel",
+        buttonClicked: function(index) {
+          var item = {
+            city: $scope.params.city,
+            lat: +$scope.params.lat,
+            lng: +$scope.params.lng
+          };
+          if (index === 0) {
+            Locations.toggle(item);
+          }
+          if (index === 1) {
+            Locations.primary(item);
+          }
+          if (index === 2) {
+            $scope.showModal();
+          }
+          return true;
         }
+      });
+    };
 
-        $scope.getTotalHeight = function () {
-            return parseInt(parseInt($scope.getHeight()) * 3) + 'px';
-        }
+    $scope.showModal = function() {
+      if ($scope.modal) {
+        $scope.modal.show();
+      } else {
+        $ionicModal
+          .fromTemplateUrl("views/weather/modal-chart.html", {
+            scope: $scope
+          })
+          .then(function(modal) {
+            $scope.modal = modal;
 
-        $scope.showOptions = function () {
-            var sheet = $ionicActionSheet.show({
-                buttons: [
-                    { text: 'Toggle Favorite' },
-                    { text: 'Set as Primary' },
-                    { text: 'Sunrise Sunset Chart' },
-                ],
-                cancelText: 'Cancel',
-                buttonClicked: function (index) {
-                    var item = {
-                        city:$scope.params.city,
-                        lat:+$scope.params.lat,
-                        lng:+$scope.params.lng,
-                    }
-                    if (index === 0) {
-                        Locations.toggle(item);
-                    }
-                    if (index === 1) {
-                        Locations.primary(item);
-                    }
-                    if (index === 2) {
-                        $scope.showModal();
-                    }
-                    return true;
-                }
-            })
-        }
-
-        $scope.showModal = function () {
-            if ($scope.modal) {
-                $scope.modal.show();
-            } else {
-                $ionicModal.fromTemplateUrl('views/weather/modal-chart.html', {
-                    scope: $scope
-                }).then(function (modal) {
-                    $scope.modal = modal;
-
-                    var days = []
-                    var day = Date.now()
-                    for (var i = 0; i < 365; i++) {
-                        day += 1000 * 60 * 60 * 24
-                        days.push(SunCalc.getTimes(day, $scope.params.lat, $scope.params.lng));
-                    }
-                    $scope.chart = days;
-
-                    $scope.modal.show();
-                });
+            var days = [];
+            var day = Date.now();
+            for (var i = 0; i < 365; i++) {
+              day += 1000 * 60 * 60 * 24;
+              days.push(
+                SunCalc.getTimes(day, $scope.params.lat, $scope.params.lng)
+              );
             }
-        };
+            $scope.chart = days;
 
-        $scope.hideModal = function () {
-            $scope.modal.hide();
-        }
+            $scope.modal.show();
+          });
+      }
+    };
 
-        $scope.$on('$destroy', function () {
-            $scope.modal.remove();
-        })
+    $scope.hideModal = function() {
+      $scope.modal.hide();
+    };
 
-
-    })
+    $scope.$on("$destroy", function() {
+      $scope.modal.remove();
+    });
+  });
